07b3086a6f19da606b51f8b4e4ff19509c536d68
Zedion03 <barcenasmarc14@gmail.com>
2025-05-09 11:36:01 +0800

    Add Team.h

---
+++ Team.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef TEAM_H
+#define TEAM_H
+
+#include <string>
+#include <unordered_map>
+#include "Hero.h"
+
+class Team {
+private:
+    std::unordered_map<std::string, Hero*> positionToHero;
+
+public:
+    void assignHeroToPosition(const std::string& position, Hero* hero);
+    void getFormattedTeam(std::vector<std::string>& lines, const std::string& teamName) const;
+
+};
+
+#endif // TEAM_H
