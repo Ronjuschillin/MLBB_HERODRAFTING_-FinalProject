4da1759f8f0cf2f11ca856fb4a4c5b7b71337051
Zedion03 <barcenasmarc14@gmail.com>
2025-05-10 11:00:21 +0800

    add case sensitive toLowerCase

--- Project1.cpp
+++ Project1.cpp
@@ -10,6 +10,11 @@
 
 
 
+std::string toLower(const std::string& str) {
+    std::string lower = str;
+    std::transform(lower.begin(), lower.end(), lower.begin(), ::tolower);
+    return lower;
+}
 
 Hero* autoPickHero(const std::string& role, const std::set<std::string>& picked) {
     const auto& pool = HeroFactory::getHeroPool();
