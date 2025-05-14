3a959bd7905e385fdebc2b092dc322af4dcb9189
Yhan-ux <moraoryan2@gmail.com>
2025-05-14 10:41:02 +0800

    Update Team.cpp

--- Team.cpp
+++ Team.cpp
@@ -4,13 +4,13 @@
 #include <windows.h>
 #include "ColorUtil.h"
 #include <sstream>
+
 void Team::assignHeroToPosition(const std::string& position, Hero* hero) {
     positionToHero[position] = hero;
 }
 
 void Team::getFormattedTeam(std::vector<std::string>& lines, const std::string& teamName) const {
    
-
     for (const auto& [position, hero] : positionToHero) {
         std::ostringstream line;
         line << "\033[0m"<< std::left << std::setw(8) << position << ":"
